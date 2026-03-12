406f9706c05217d6d2fa3c78e0b54c76fa9417fd
Krishna <krishnaku089@gmail.com>
2026-03-12 22:06:17 +0530

    setup auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(() => {
+    return localStorage.getItem("authUser");
+  });
+
+  const login = (username, password) => {
+    if (username === "testuser" && password === "Test123") {
+      setUser(username);
+      localStorage.setItem("authUser", username);
+      return true;
+    }
+    return false;
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("authUser");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
